6a51fd71df9e7ea2519811cee12a8fb0a2027706
Andrey Kostadinov <69603223+DarthSithRevan@users.noreply.github.com>
2025-12-19 06:57:14 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,3 +4,6 @@ upload.addEventListener("change", e => {
   const file = e.target.files[0];
   if (file) openBook(file);
 });
+document.getElementById("toggleTheme").onclick = () => {
+  document.body.classList.toggle("dark");
+};
