bd331450041f5e0c57a32fc1711e642556e1d991
Andrey Kostadinov <69603223+DarthSithRevan@users.noreply.github.com>
2025-12-19 06:55:47 +0200

    Update reader.js

--- reader.js
+++ reader.js
@@ -1,33 +1,19 @@
 let rendition;
-let currentBookId;
+let bookKey;
 
-async function openBook(url, bookId) {
-  currentBookId = bookId;
+function openBook(file) {
+  bookKey = `epub-progress-${file.name}`;
 
-  const book = ePub(url);
+  const book = ePub(file);
   rendition = book.renderTo("reader", {
     width: "100%",
-    height: "100%",
-    theme: "dark"
+    height: "100%"
   });
 
-  const { data } = await supabase
-    .from("reading_progress")
-    .select("cfi")
-    .eq("book_id", bookId)
-    .single();
-
-  rendition.display(data?.cfi || undefined);
+  const savedCfi = localStorage.getItem(bookKey);
+  rendition.display(savedCfi || undefined);
 
   rendition.on("relocated", loc => {
-    saveProgress(loc.start.cfi);
-  });
-}
-
-async function saveProgress(cfi) {
-  await supabase.from("reading_progress").upsert({
-    book_id: currentBookId,
-    user_id: (await supabase.auth.getUser()).data.user.id,
-    cfi
+    localStorage.setItem(bookKey, loc.start.cfi);
   });
 }
